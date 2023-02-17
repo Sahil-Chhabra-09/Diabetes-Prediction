e4976b501d8c6ae2330c520268cac34e49f02b44
Sahil Chhabra <122990320+Sahil-Chhabra-09@users.noreply.github.com>
2023-02-17 18:30:00 +0530

    Update Diabetes_Prediction_Webapp.py

--- Diabetes_Prediction_Webapp.py
+++ Diabetes_Prediction_Webapp.py
@@ -25,14 +25,14 @@ def main():
     # getting the input data from the user
         #Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age
     
-    Pregnancies = st.text_input("Number of Pregnancies")
-    Glucose = st.text_input("Glucose level")
-    Bloodpressure = st.text_input("Blood Pressure")
-    SkinThickness = st.text_input("Skin Thickness")
-    Insulin = st.text_input("Insulin levels")
-    BMI = st.text_input("Body Mass Index")
-    DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
-    Age = st.text_input("Age of person")
+    Pregnancies = st.text_input("Number of Pregnancies (0-17)")
+    Glucose = st.text_input("Glucose level (0-199)")
+    Bloodpressure = st.text_input("Blood Pressure (0-122)")
+    SkinThickness = st.text_input("Skin Thickness (0-99)")
+    Insulin = st.text_input("Insulin levels (0-846)")
+    BMI = st.text_input("Body Mass Index (0.0-67.1)")
+    DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value (0.078-2.42)")
+    Age = st.text_input("Age of person (21-81)")
     
     # code for Prediction
     diagnosis = ''
