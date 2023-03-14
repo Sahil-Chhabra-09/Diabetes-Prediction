29e1cf5c57e3d01a551bffaeb829ac1b0f39c1e5
Sahil Chhabra <122990320+Sahil-Chhabra-09@users.noreply.github.com>
2023-03-15 00:50:42 +0530

    added navigation

--- Diabetes_Prediction_Webapp.py
+++ Diabetes_Prediction_Webapp.py
@@ -17,32 +17,60 @@ def diabetes_prediction(input_data):
         return "The person is diabetic"
     
 def main():
+    page = st.selectbox('Select what you see',('Homepage', 'Data Analysis', 'Data Set'))
     
-    #giving a title
-    st.title('Diabetes Prediction Web App')
+    if page == 'HomePage':
     
-    # getting the input data from the user
-        #Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age
-    
-    Pregnancies = st.text_input("Number of Pregnancies (0-17)")
-    Glucose = st.text_input("Glucose level (0-199)")
-    Bloodpressure = st.text_input("Blood Pressure (0-122)")
-    SkinThickness = st.text_input("Skin Thickness (0-99)")
-    Insulin = st.text_input("Insulin levels (0-846)")
-    BMI = st.text_input("Body Mass Index (0.0-67.1)")
-    DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value (0.078-2.42)")
-    Age = st.text_input("Age of person (21-81)")
-    
-    # code for Prediction
-    diagnosis = ''
-    
-    # creating a button for prediction
-    if st.button("Diabetes Test Result"):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, Bloodpressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        #giving a title
+        st.title('Diabetes Prediction Web App')
+
+        # getting the input data from the user
+            #Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age
+
+        Pregnancies = st.text_input("Number of Pregnancies (0-17)")
+        Glucose = st.text_input("Glucose level (0-199)")
+        Bloodpressure = st.text_input("Blood Pressure (0-122)")
+        SkinThickness = st.text_input("Skin Thickness (0-99)")
+        Insulin = st.text_input("Insulin levels (0-846)")
+        BMI = st.text_input("Body Mass Index (0.0-67.1)")
+        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value (0.078-2.42)")
+        Age = st.text_input("Age of person (21-81)")
+
+        # code for Prediction
+        diagnosis = ''
+
+        # creating a button for prediction
+        if st.button("Diabetes Test Result"):
+            diagnosis = diabetes_prediction([Pregnancies, Glucose, Bloodpressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+
+
+        st.success(diagnosis)
         
         
-    st.success(diagnosis)
-    
+    elif page == 'Data Analysis':
+        st.title("Exploratory Data Analysis")
+        df = pd.read_csv(r"C:\Users\Sahil Chhabra\Desktop\Streamlit\dataset.csv")
+        Outcome = df['Outcome']
+        df.drop("Outcome", inplace=True, axis = 1)
+        columns = st.radio(
+            "Data to show:",
+            (df.columns),
+            horizontal=True
+        )
+        plt.grid()
+        st.line_chart(df[columns])
+        st.pyplot(sns.histplot(df[columns]).figure)
+        show = st.checkbox("Show Data Description")
+        if show:
+            st.write("Description of data")
+            st.write(df.describe())
+        
+        
+    elif page=='Data Set':
+        st.title("Here's the dataset on which ML model is based")
+        df = pd.read_csv(r"C:\Users\Sahil Chhabra\Desktop\Streamlit\dataset.csv")
+        # print(df.head())
+        st.table(df)
     
     
     
